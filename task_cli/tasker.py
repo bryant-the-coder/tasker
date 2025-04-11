3b7d4158a16848983e05e9bd6cbb6043da93554f
bryant-the-coder <foobryant99@gmail.com>
2025-04-11 22:03:49 +0800

    rewrite functions

--- task_cli/tasker.py
+++ task_cli/tasker.py
@@ -1,22 +1,46 @@
 import json
 import os
+from datetime import datetime
 
 filename = os.path.expanduser("~/tasker.json")
 
 
-def save_data(data: dict[str, dict], path: str) -> None:
-    with open(path, "w") as file:
+def __init__(self):
+    self.data = self.load_data()
+
+
+def save_tasks(data):
+    with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
 
-def load_data(path: str) -> dict[str, dict]:
-    try:
-        with open(path) as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        data = {}
-    return data
+def load_tasks():
+    if not os.path.isfile(filename):
+        with open(filename, "w") as file:
+            json.dump([], file)
+        return []
+
+    with open(filename, "r") as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return []
+
+
+def get_id(tasks):
+    # Creates a list of all IDs
+    # Finds the highest number (id) then add 1
+    # default starts from 0
+    return max([task["id"] for task in tasks], default=0) + 1
+
+
+def add_task(description):
+    tasks = load_tasks()
+    id = get_id(tasks)
+    currentTime = datetime.now().isoformat()
+    status = "Todo"
+    print(f"Task added successfully (ID: {id})")
 
 
 def main():
-    print("testing")
+    print("Welcome to Tasker")
